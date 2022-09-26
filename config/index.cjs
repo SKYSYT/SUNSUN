6e11a3610348d112a6b081c5bdab074acb16bf91
SKYSYT <114211645+SKYSYT@users.noreply.github.com>
2022-09-26 20:17:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDoGd56mLLw8k0DVojPXHH7JVTyM',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'I9YQtNySXvaF2N2N2o-g5zFxvYOne5LpbI4Ju6hBsqw',
+      useTemplateId: 'mhpL-EMCsYXen-u-rWyI7q2Q0GsFmH16UxKcFYSOhXw',
       // 所在省份
       province: '北京',
       // 所在城市
