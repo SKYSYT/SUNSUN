dc0c962ff2598974697bf8df33f4c600c0685964
SKYSYT <114211645+SKYSYT@users.noreply.github.com>
2022-09-26 19:52:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -104,6 +104,7 @@ const USER_CONFIG = {
           type: '节日', name: '小媳妇阳历生日', year: '2004', date: '05-23',
         }
       ],
+        FESTIVALS_LIMIT: 3,
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
